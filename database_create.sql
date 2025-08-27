59d3f03a7240dc8e2a2d690fecc566b7fb904590
Vipula Liyanaarachchi <vipulasrilanka@yahoo.com>
2025-08-28 01:08:04 +0530

    Set sessions id as primery key

--- database_create.sql
+++ database_create.sql
@@ -388,7 +388,7 @@ INSERT INTO `roles` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
 --
 
 CREATE TABLE `sessions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `sessionName` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `createdByUser` int(11) DEFAULT NULL,
@@ -634,12 +634,6 @@ ALTER TABLE `refreshTokens`
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `sessions`
---
-ALTER TABLE `sessions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`createdByUser`);
 
 --
 -- Indexes for table `session_properties`
